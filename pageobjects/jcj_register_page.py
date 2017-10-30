6aa1b3aa43a66be38e5679dbc5ffee13c5d049a5
root <root@beta.dev>
2017-10-30 11:20:02 +0800

    增加注册页面元素资源 注册页面类方法定义

---
+++ pageobjects/jcj_register_page.py
@@ -0,0 +1,58 @@
+# coding = utf-8
+
+from framework.base_base import BasePage
+
+import os
+import json
+
+
+class RegisterPage(BasePage):
+    # 注册页面
+
+    def __init__(self, driver):
+        super(RegisterPage, self).__init__(driver)
+
+        # load json elements
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "elements_top.json"),"r", encoding="utf-8") as f:
+            self.map_top = json.load(f)
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "elements_login.json"), "r", encoding="utf-8") as f:
+            self.map_register = json.load(f)
+
+        self.to_register()      
+
+        def to_register(self):
+            element = self.find_the_element(self.map_register['link_register'])
+
+        def input_reg_phone(self, phone):
+            self.typeIn(self.map_register['input_reg_phone'], phone)
+            time.sleep(1)
+
+        def get_phone_warn(self):
+            return self.find_the_element(self.map_register['input_reg_phone']).get_attribute('value')
+
+        def input_reg_password(self, password):
+            self.typeIn(self.map_register['input_reg_password'], password)
+            time.sleep(1)
+
+        def get_password_warn(self):
+            reutrn self.find_the_element(self.map_register['input_reg_password']).get_attribute('value')
+
+        def click_get_sms(self):
+            self.click(self.map_register['button_get_sms'])
+            time.sleep(1)
+
+        def input_verify_code(self,code):
+            self.typeIn(self.map_register['input_reg_sms'])
+            time.sleep(1)
+
+        def get_verify_warn(self):
+            return self.find_the_element(self.map_register['input_reg_sms']).get_attribute('value')
+
+        def check_reg_protocol(self):
+            self.click(self.map_register['check_reg_protocol'])
+        
+        def click_reg_confirm(self):
+            self.click(self.map_register['button_reg_confirm'])
+
+        def get_regok_msg(self):
+            return self.find_the_element(self.map_register['text_reg_ok']).text
